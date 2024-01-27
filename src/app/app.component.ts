5537429fa925328e3c4abdc98d47db335bec1a76
Juan Bello <juan.obello@gmail.com>
2024-01-27 15:44:40 -0300

    route conditional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipe-app-angular';
+  path = 'recipes';
+
+  onNavigateTo(pathTo: string) {
+    this.path = pathTo;
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  @Output() navigateTo = new EventEmitter<string>();
+
+  onNavigate(path: string) {
+    this.navigateTo.emit(path);
+  }
+
+  ngOnInit(): void {}
+}
